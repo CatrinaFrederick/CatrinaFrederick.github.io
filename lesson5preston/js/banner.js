c08641f5af0841497454badbcf9184f519832004
cfrederickc <gfrederick@palomar.edu>
2020-02-10 13:04:55 -0800

    so close

--- lesson5preston/js/banner.js
+++ lesson5preston/js/banner.js
@@ -3,7 +3,7 @@
 let today = new Date();
 let dayofWeek = today.getDay();
 
-let banner = document.getElementById("pancakebreakfast");
+let banner = document.getElementById("pancake");
 
 if (dayofWeek == 5) {
     banner.classList.add("showme");
