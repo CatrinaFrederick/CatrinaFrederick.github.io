8db79c702c9564db394b6b5496a22f02da9a9e2d
cfrederickc <gfrederick@palomar.edu>
2020-03-02 12:12:06 -0800

    push

--- lesson 8/js/togglemenu.js
+++ lesson 8/js/togglemenu.js
@@ -3,3 +3,8 @@
 function toggleMenu() {
     document.getElementById("mainNav").classList.toggle("hide");
 }
+
+/*------adjust rating-------*/
+function adjustRating() {
+    document.getElementById("ratingvalue").innerHTML = document.getElementById("rating").value;
+}
