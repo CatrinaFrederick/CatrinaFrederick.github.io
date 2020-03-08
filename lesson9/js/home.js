c6dd23d905cda57ced666f681a3a829313b1fb17
cfrederickc <gfrederick@palomar.edu>
2020-03-08 11:54:00 -0700

    push

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -12,14 +12,14 @@ fetch(requestURL)
         if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven"){
    //create html elements       
         let card = document.createElement('section');
-        let h2 = document.createElement('h2');
+        let h2 = document.createElement('h2'); 
         let h3 =document.createElement('h3');
         let year = document.createElement('p');
         let pop = document.createElement('p')
         let rain = document.createElement('p');
         let image = document.createElement('img');
    //assign content to the elements 
-        h2.textContent = towns[i].name;
+        h2.textContent = towns[i].name; 
         h3.textContent = towns[i].motto;
         year.textContent = 'Year Founded: ' + towns[i].yearFounded;
         pop.textContent = 'Population: ' + towns[i].currentPopulation;
@@ -28,8 +28,9 @@ fetch(requestURL)
 
 
    //Append content to HTML 
-        card.appendChild(h2);
-        card.appendChild(h3);
+     
+        card.appendChild(h2); 
+        card.appendChild(h3); 
         card.appendChild(year)
         card.appendChild(pop);
         card.appendChild(rain);
